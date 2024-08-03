d3f2ef927e3e22aafb4cd5a4ae18a4b303d2f7bd
thebodegavarshithsai <teenu5456@gmail.com>
2024-08-04 02:26:28 +0530

    frontend

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+import all_product from "../Components/Assets/all_product"
+
+export const ShopContext=createContext(null)
+
+const ShopContextProvider=({children})=>{
+  return(
+    <ShopContext.Provider value={all_product}>
+      {children}
+    </ShopContext.Provider>
+  )
+}
+export default ShopContextProvider;
